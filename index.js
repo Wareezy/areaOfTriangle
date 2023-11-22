9c78b031ef1ed1fa31e237d59ed75cc4d35f948a
Wareezy <warrenjaftha16@gmail.com>
2023-11-22 09:30:06 +0200

    warrenLC

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ let divider=0.5
 let btnMulti=document.querySelector('[data-calc]')
 
 //function that will be performing the calculation
-function area(){
+    function area(){
     let input= document.querySelector('[data-input]').value
     let input2= document.querySelector('[data-input2]').value
+
     //backticks are used to access the variables and to display the data more neatly
-    let answer=eval(`(${input} * ${input2})*divider`)
+    let answer=eval(`(${input} * ${input2})*${divider}`)
     answer=answer.toFixed(2);
-    display.innerHTML=answer
+
+    //when displaying into a input tag you should use .value
+    display.value=answer
 }
+
 //calling a click function. so that when the button is clicked that it accesses the area function
 btnMulti.addEventListener('click',area)
